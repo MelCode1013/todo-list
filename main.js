a3e901762a52811e8da08424907a621a75d1bc3e
Melinna <Melinna.art@gmail.com>
2022-03-26 14:49:20 -0400

    created alert if nothing is entered, else statement for success

--- main.js
+++ main.js
@@ -1,5 +1,17 @@
 window.addEventListener('load', () => {
-    const form = document.querySelector("#new-task-form";
+    const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
-    const list_el = document.querySelector("#task-list");
+    const list_el = document.querySelector("#tasks");
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+
+        const task = input.value
+
+        if (!task) {
+            alert("Please fill out your to do list");
+        } else {
+            console.log("Success!");
+        }
+    })
 })
